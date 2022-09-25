f628a94953e675de83d8c9a7c58775eb88e2c3d0
YugiBird <yugalnandurkar@gmail.com>
2022-09-26 03:36:16 +0530

    Update app.py

--- 1-Churn-Predictor-Model/1-Telco-Services-Application/app.py
+++ 1-Churn-Predictor-Model/1-Telco-Services-Application/app.py
@@ -193,7 +193,7 @@ def main():
                             0: "No, the customer is happy with Telco Services.",
                         }
                     )
-                  else:
+                 else:
                       st.write("Proceed To Upload Indispensable Model")
 
                 st.markdown("<h3></h3>", unsafe_allow_html=True)
